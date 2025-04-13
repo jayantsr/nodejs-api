16951e31ea8678edd791e7ef58f088b2907d2493
Jayan Varma <Jayantsr@gmail.com>
2025-04-13 07:53:06 +0530

    first-test-apprunner

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 const express = require("express");
 const app = express();
 app.get("/", (req, res) => {
-  res.send("Hello World");
+  res.send("Hello World - We are on AWS APP Runner");
 });
 
 app.get("/api/endpoint", (req, res) => {
